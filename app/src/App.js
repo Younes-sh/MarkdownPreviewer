69d3230e8197c2f3c3f2141e7020bdd454669ea8
younes sheikhlar <sheikhlaryounes@mail.com>
2022-09-16 22:52:07 +0200

    create react app and install react-markdown-editor

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import MarkdownEditor from '@uiw/react-markdown-editor';
+function App() {
+  const [markdown, setMarkdown] = useState("");
+
+  return (
+    
+      <MarkdownEditor
+      value="# This is a H1  \n## This is a H2  \n###### This is a H6"
+      onChange={(value, viewUpdate) => setMarkdown(value)}
+    />
+
+    
+  );
+}
+
+export default App;
